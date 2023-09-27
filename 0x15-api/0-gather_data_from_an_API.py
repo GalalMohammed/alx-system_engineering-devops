ea3ecd20d2e7d2375382d1dffa52e1e90bf7ed41
GalalMohammed <galalmoh2001@gmail.com>
2023-09-27 21:22:51 +0300

    add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""gets information about his/her TODO list progress.
+
+Example:
+    $ python3 0-gather_data_from_an_API.py 2
+
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    # Fetch TODO list and user name
+    todo = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}'
+            ).json()
+    user_name = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+            ).json()['name']
+    completed_tasks = [task for task in todo if task['completed']]
+    print(f'Employee {user_name} is done with tasks' +
+          f'({len(completed_tasks)}/{len(todo)}):')
+    for task in completed_tasks:
+        print('\t ' + task['title'])
