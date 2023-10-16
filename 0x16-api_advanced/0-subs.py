0ff61bc714348633010cfa5729a97ea5cd530dc6
GalalMohammed <galalmoh2001@gmail.com>
2023-10-16 13:26:27 +0300

    add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""define number_of_subscribers function.
+
+Example:
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit: str) -> int:
+    """Query the Reddit API and returns the number of subscribers.
+
+    Args:
+        subreddit (str): given subreddit.
+
+    Returns:
+        int.
+
+    """
+    # Send a GET request to the Reddit API
+    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                            timeout=1)
+    return response.json().get("data").get("subscribers", 0)
