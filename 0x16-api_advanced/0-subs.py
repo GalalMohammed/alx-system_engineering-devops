ce68aa58b8b788c2c4b7634dbc9bfe157659ea78
GalalMohammed <galalmoh2001@gmail.com>
2023-10-16 13:38:41 +0300

    fix import order

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """define number_of_subscribers function.
-
-Example:
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-
 """
 
 import requests
@@ -21,5 +17,5 @@ def number_of_subscribers(subreddit: str) -> int:
     """
     # Send a GET request to the Reddit API
     response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                            timeout=1, headers={"User-Agent": 'Galal'})
+                            timeout=1, headers={"User-Agent": "Galal"})
     return response.json().get("data", {}).get("subscribers", 0)
