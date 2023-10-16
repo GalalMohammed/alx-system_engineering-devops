0c223a1790b4c463e1aebfd58424e6d4becf0f6b
GalalMohammed <galalmoh2001@gmail.com>
2023-10-16 14:00:18 +0300

    add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""define top_ten function.
+"""
+
+import requests
+
+
+def top_ten(subreddit: str):
+    """Query the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): given subreddit.
+    """
+    # Send a GET request to the Reddit API
+    response = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json",
+                            timeout=1, headers={"User-Agent": "Galal"})
+    posts = response.json().get("data", {}).get("children")
+    if posts:
+        for post in posts[:10]:
+            print(post["data"]["title"])
+    else:
+        print(None)
